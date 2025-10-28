c9243bf674f76e864bcdbab8503929a713235a48
FDims <110996703+FDims@users.noreply.github.com>
2025-10-28 13:15:07 +0100

    homepage and header fix (#2)

--- app/component/header/header.vue
+++ app/component/header/header.vue
@@ -23,26 +23,47 @@ const items = computed<NavigationMenuItem[]>(() => [
 </script>
 
 <template>
-  <UHeader class="header">
+  <UHeader
+    class="header"
+    toggle-side="left"
+    :ui="{
+    toggle: 'flex sm:hidden',
+    }"
+  >
     <template #left>
       <img
         src="~/assets/images/aachen-open-logo.png"
         alt="logo"
-        class="header-logo"
+        class="header-logo size-15"
       />
-      <h1 class="header-text">
+      <h1 class="header-text text-2xl font-bold">
         Aachen Open
       </h1>
     </template>
 
     <template #right>
+      <div class="hidden sm:flex">
+        <UNavigationMenu
+          :items="items"
+          color="neutral"
+          highlight
+          class="header-menu"
+          :ui="{
+            link: 'header-menu'
+          }"
+        />
+      </div>
+    </template>
+
+    <template #body>
       <UNavigationMenu
         :items="items"
         color="neutral"
         highlight
         class="header-menu"
+        orientation="vertical"
         :ui="{
-          link: 'header-menu'
+          link: 'header-menu text-xl'
         }"
       />
     </template>
