9b3510b1fce661e9b4f6a21f110728659f7db93a
FachrialDImas <110996703+FDims@users.noreply.github.com>
2025-11-04 23:57:40 +0100

    adjustments (#9)
    sizing and name adjustment
    
    disable link
    
    badminton mixed double and schedule table and fixes (#9)
    
    text and style adjustment
    
    redirect fix
    
    Co-Authored-By: FachrialDImas <110996703+FDims@users.noreply.github.com>

--- app/component/standings/standingsPlayoff/standingPlayoff.vue
+++ app/component/standings/standingsPlayoff/standingPlayoff.vue
@@ -17,6 +17,8 @@ interface BracketData {
   final: Schedule[];
 }
 
+console.log(props.matchSchedules)
+
 const bracketData: BracketData ={
   quarterFinals: props.matchSchedules.filter((match) => match.type == "quarter"),
   semiFinals: props.matchSchedules.filter((match) => match.type == "semi"),
