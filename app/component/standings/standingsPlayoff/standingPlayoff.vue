f303c6bff30e55bec86299613c06d0e8fee1f802
FPerdana <110996703+FDims@users.noreply.github.com>
2025-11-06 14:42:52 +0100

    Update standingPlayoff.vue

--- app/component/standings/standingsPlayoff/standingPlayoff.vue
+++ app/component/standings/standingsPlayoff/standingPlayoff.vue
@@ -17,8 +17,6 @@ interface BracketData {
   final: Schedule[];
 }
 
-console.log(props.matchSchedules)
-
 const bracketData: BracketData ={
   quarterFinals: props.matchSchedules.filter((match) => match.type == "quarter"),
   semiFinals: props.matchSchedules.filter((match) => match.type == "semi"),
