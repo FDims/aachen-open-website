f47c0dfd1a26a87a42b927a708424d5f43c415d7
FachrialDImas <110996703+FDims@users.noreply.github.com>
2025-11-25 01:23:31 +0100

    merch

---
+++ app/assets/images/index.ts
@@ -0,0 +1,35 @@
+//sports logo
+export {default as badmintonLogo} from "./badminton_logo.svg";
+export {default as futsalLogo} from "./football_logo.svg";
+export {default as volleyballLogo} from "./volleyball_logo.svg";
+export {default as basketballLogo} from "./basketball_logo.svg";
+export {default as dateLogo} from "./date_logo.svg";
+
+//org logo
+export {default as ppiLogo} from "./ppi_logo.svg";
+export {default as makLogo} from "./mak_logo.svg";
+export {default as vcwsaLogo} from "./vcwsa_logo.svg";
+export {default as instagramIcon} from "./instagram_icon.svg";
+export {default as locationIcon} from "./location_logo.svg";
+
+//merch bundle pics
+export {default as bundle1} from "./merch/bundle/bundle1.jpg";
+export {default as bundle2} from "./merch/bundle/bundle2.jpg";
+export {default as bundle3} from "./merch/bundle/bundle3.jpg";
+export {default as bundle4} from "./merch/bundle/bundle4.jpg";
+
+//merch bundle pics
+export {default as jersey1} from "./merch/jersey/jersey1.jpg";
+export {default as jersey2} from "./merch/jersey/jersey2.jpg";
+export {default as jersey3} from "./merch/jersey/jersey3.jpg";
+export {default as jersey4} from "./merch/jersey/jersey4.jpg";
+export {default as jersey5} from "./merch/jersey/jersey5.jpg";
+
+//merch tshirt pics
+export {default as tshirt1} from "./merch/t-shirt/t1.jpg";
+export {default as tshirt2} from "./merch/t-shirt/t2.jpg";
+export {default as tshirt3} from "./merch/t-shirt/t3.jpg";
+export {default as tshirt4} from "./merch/t-shirt/t4.jpg";
+export {default as tshirt5} from "./merch/t-shirt/t5.jpg";
+export {default as tshirt6} from "./merch/t-shirt/t6.jpg";
+export {default as tshirt7} from "./merch/t-shirt/t7.jpg";
---
+++ app/pages/merch.vue
@@ -0,0 +1,188 @@
+<script setup lang="ts">
+import "../assets/styles/pages/merch.scss";
+import { useBreakpoints, breakpointsTailwind } from '@vueuse/core';
+import {
+  bundle1,
+  bundle2,
+  bundle3,
+  bundle4,
+  jersey1,
+  jersey2,
+  jersey3,
+  jersey4,
+  jersey5,
+  tshirt1, tshirt2, tshirt3, tshirt4, tshirt5, tshirt6, tshirt7
+} from "~/assets/images";
+
+const breakpoints = useBreakpoints(breakpointsTailwind)
+
+const eventDate = new Date(2025, 11, 6);
+const today = new Date();
+console.log(today)
+console.log(eventDate)
+
+const disable = () : boolean => {
+  if(today.getFullYear() < eventDate.getFullYear())
+    return false;
+  if(today.getMonth() < eventDate.getMonth())
+    return false;
+  return today.getDate() >= eventDate.getDate();
+
+};
+
+const bundlePhotos = [
+    bundle3,
+    bundle1,
+    bundle4,
+    bundle2,
+]
+
+const jerseyPhotos = [
+    jersey1,
+    jersey2,
+    jersey3,
+    jersey4,
+    jersey5
+]
+
+const tShirtPhotos = [
+    tshirt1,
+    tshirt2,
+    tshirt3,
+    tshirt4,
+    tshirt5,
+    tshirt6,
+    tshirt7,
+]
+
+const ctaOrientation = computed(() => {
+  return breakpoints.greaterOrEqual('md').value ? 'horizontal' : 'vertical'
+})
+
+</script>
+
+<template>
+  <div>
+    <div class="merch-text">
+      <h1 class="merch-title">Aachen Open Merch Pre-Order</h1>
+      <p class="merch-subtitle">pre-order closed on 5th December 2025, after that date the price will not be discounted</p>
+    </div>
+
+
+    <UPageCTA
+        :orientation="ctaOrientation"
+        reverse
+        class="bundle-wrapper"
+    >
+      <template #title>
+        <h1 class="bundle-title"> Jersey and T-Shirt Bundle</h1>
+      </template>
+      <template #description>
+        <p class="bundle-subtitle">Pre-Order now for:</p>
+        <div class="bundle-prices">
+          <p class="bundle-price">€22</p>
+          <p class="bundle-price-original">€27</p>
+        </div>
+
+      </template>
+      <template #links>
+        <ULink
+            href="https://docs.google.com/forms/d/e/1FAIpQLSdN3oo6iMPntX_5q5CaEMXHCM2lvDwc6iHK1Zky-gN1F6CPeQ/viewform"
+            target="_blank"
+            class="bundle-buy-button"
+            :disabled="disable()"
+        >
+          <UButton color="neutral" variant="soft" :disabled="disable()">
+            Order now
+          </UButton>
+        </ULink>
+      </template>
+      <UCarousel
+          v-slot="{item}"
+          arrows
+          :items="bundlePhotos"
+          class="bundle-carousel"
+          loop
+      >
+        <img
+            :src="item"
+            alt="bundle-photo"
+            class="rounded-lg bundle-pictures"
+        />
+      </UCarousel>
+    </UPageCTA>
+
+
+    <div class="merch-individual">
+      <UPageCard class="merch-jersey" reverse orientation="vertical">
+          <h1 class="jersey-title">Jersey</h1>
+          <p class="jersey-subtitle">Pre-Order now for:</p>
+          <div class="jersey-prices">
+            <p class="jersey-price">€12</p>
+            <p class="jersey-price-original">€15</p>
+          </div>
+          <ULink
+              href="https://docs.google.com/forms/d/e/1FAIpQLSdN3oo6iMPntX_5q5CaEMXHCM2lvDwc6iHK1Zky-gN1F6CPeQ/viewform"
+              target="_blank"
+              class="jersey-buy-button"
+              :disabled="disable()"
+          >
+            <UButton color="neutral" variant="soft" :disabled="disable()">
+              Order now
+            </UButton>
+          </ULink>
+        <UCarousel
+            v-slot="{item}"
+            arrows
+            :items="jerseyPhotos"
+            class="jersey-carousel"
+            loop
+        >
+          <img
+              :src="item"
+              alt="jersey-photo"
+              class="rounded-lg jersey-pictures"
+          />
+        </UCarousel>
+      </UPageCard>
+
+      <UPageCard class="merch-tshirt" reverse orientation="vertical">
+        <h1 class="tshirt-title">T-Shirt</h1>
+          <p class="tshirt-subtitle">Pre-Order now for:</p>
+          <div class="tshirt-prices">
+            <p class="tshirt-price">€13</p>
+            <p class="tshirt-price-original">€16</p>
+          </div>
+          <ULink
+              href="https://docs.google.com/forms/d/e/1FAIpQLSdN3oo6iMPntX_5q5CaEMXHCM2lvDwc6iHK1Zky-gN1F6CPeQ/viewform"
+              target="_blank"
+              class="tshirt-buy-button"
+              :disabled="disable()"
+          >
+            <UButton color="neutral" variant="soft" :disabled="disable()">
+              Order now
+            </UButton>
+          </ULink>
+        <UCarousel
+            v-slot="{item}"
+            arrows
+            :items="tShirtPhotos"
+            class="tshirt-carousel"
+            loop
+        >
+          <img
+              :src="item"
+              alt="tshirt-photo"
+              class="rounded-lg tshirt-pictures"
+          />
+        </UCarousel>
+      </UPageCard>
+    </div>
+
+
+  </div>
+</template>
+
+<style scoped>
+
+</style>
