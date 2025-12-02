3db9ca02c9a7c2e06b4339dc4a62ecb37ba52f3f
FachrialDImas <110996703+FDims@users.noreply.github.com>
2025-12-02 23:26:57 +0100

    playoff fix

--- app/component/standings/standingsMain/standingsMain.vue
+++ app/component/standings/standingsMain/standingsMain.vue
@@ -88,7 +88,7 @@ const getPlayoffTeams = (sportType: Sport): Schedule[] => {
         <standing-group :groups="getSportGroup(badmintonMenDoubleTeams)" :playoff="getPlayoffTeams('badmintonMenDouble')" bracket="quarter"/>
       </template>
       <template #badmintonMixedDouble>
-        <standing-group :groups="getSportGroup(badmintonMixedDoubleTeams)" :playoff="getPlayoffTeams('badmintonMixedDouble')" bracket="quarter"/>
+        <standing-group :groups="getSportGroup(badmintonMixedDoubleTeams)" :playoff="getPlayoffTeams('badmintonMixedDouble')" bracket="semi"/>
       </template>
       <template #basketball>
         <standing-group :groups="getSportGroup(basketballTeams)" :playoff="getPlayoffTeams('basketball')" bracket="semi"/>
@@ -97,7 +97,7 @@ const getPlayoffTeams = (sportType: Sport): Schedule[] => {
         <standing-group :groups="getSportGroup(volleyballTeams)" :playoff="getPlayoffTeams('volleyball')" bracket="semi"/>
       </template>
       <template #futsal>
-        <standing-group :groups="getSportGroup(futsalTeams)" :playoff="getPlayoffTeams('futsal')" bracket="semi"/>
+        <standing-group :groups="getSportGroup(futsalTeams)" :playoff="getPlayoffTeams('futsal')" bracket="quarter"/>
       </template>
     </UTabs>
   </div>
